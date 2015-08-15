25953173403cdab1e1c5a47c4d003522653848a0
Wael <ymnwael@gmail.com>
2015-08-15 14:19:45 -0600

    login pages meta data changes -* commit message change -> config.ini changes enable admin login in reports page. fake db creditentials

--- login_reports.php
+++ login_reports.php
@@ -39,9 +39,9 @@ if (isset($_SESSION['valid_reports_user'])) {
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="Creative - Bootstrap 3 Responsive Admin Template">
-    <meta name="author" content="GeeksLabs">
-    <meta name="keyword" content="Creative, Dashboard, Admin, Template, Theme, Bootstrap, Responsive, Retina, Minimal">
+    <meta name="description" content="PHP TimeClock, Admin">
+    <meta name="author" content="Wael Ali">
+    <meta name="keyword" content="PHP TimeClock, Bootstrap, Responsive">
     <link rel="shortcut icon" href="img/favicon.png">
 
     <title>Admin Login</title>
